57a6f9c59b26ad7aa1d2b37483f03f0d7d3bfdda
Kirill Bondarenko <reg.kirillb.zp@gmail.com>
2024-02-22 02:43:52 +0200

    Validation, UserName in header

--- Shared/DataTransferObjects/UserProfileDto.cs
+++ Shared/DataTransferObjects/UserProfileDto.cs
@@ -1,7 +1,10 @@
-﻿namespace OpenGitSync.Shared.DataTransferObjects
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OpenGitSync.Shared.DataTransferObjects
 {
     public class UserProfileDto
     {
+        [Required(ErrorMessage = "Name is required")]
         public string UserName { get; set; }
         public string Email { get; set; }
     }
